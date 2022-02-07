ff47cc3c52a6a65e08aa1c70f75e4b386d649521
Vaimer9 <vaim3r9@gmail.com>
2022-02-07 11:37:00 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Pmake
+
+This is a small bash script I personally use to create file trees for C, Cpp and Haskell projects. I've spent 5 minutes of my life making it portable might as well give it a try
---
+++ pmake.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+function helpmsg() {
+	echo "pmake <language> <project_name>"
+	echo "Available languages are:"
+	echo "1. clang    C"
+	echo "2. cpp      C++"
+	echo "3. hsk      Haskell"
+}
+
+function createtree() {
+	case $1 in
+		clang)
+				if [ -d ~/.config/pmake/C ]
+				then
+					cp -r ~/.config/pmake/C $2
+				else
+					mkdir ~/.config/pmake/C
+					createtree $1 $2
+				fi
+			;;
+		cpp)
+				if [ -d ~/.config/pmake/Cpp ]
+				then
+					cp -r ~/.config/pmake/Cpp $2
+				else
+					mkdir ~/.config/pmake/Cpp
+					createtree $1 $2
+				fi
+			;;
+		hsk)
+				if [ -d ~/.config/pmake/Haskell ]
+				then
+					cp -r ~/.config/pmake/Haskell $2
+				else
+					mkdir ~/.config/pmake/Haskell
+					createtree $1 $2
+				fi
+			;;
+		*)
+			helpmsg
+			;;
+	esac
+}
+
+function main() {
+	if [ -d ~/.config/pmake ]
+	then
+		createtree $1 $2
+	else
+		mkdir ~/.config/pmake
+		main $1 $2
+	fi
+}
+
+if [ -z "$1" ] || [ -z "$2" ]
+then
+	helpmsg
+else
+	main $1 $2
+fi
